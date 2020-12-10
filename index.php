81eb480fc03165d98429ee6b2c86133bb515e61a
nikharshah <75773826+nikharshah@users.noreply.github.com>
2020-12-10 15:48:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,3 +14,43 @@
 	</div>
 <script src="jquery-3.2.1.min.js"></script>
 <script>
+	function show_poll(){
+		$.ajax({
+			type: "POST", 
+			url: "show-poll.php", 
+			processData : false,
+			beforeSend: function() {
+				$("#overlay").show();
+			},
+			success: function(responseHTML){
+				$("#overlay").hide();
+				$("#poll-content").html(responseHTML);
+			}
+		});
+	}
+
+	function addPoll() {
+		if($("input[name='answer']:checked").length != 0){
+			var answer = $("input[name='answer']:checked").val();
+			$.ajax({
+				type: "POST", 
+				url: "save-poll.php", 
+				data : "question="+$("#question").val()+"&answer="+$("input[name='answer']:checked").val(),
+				processData : false,
+				beforeSend: function() {
+					$("#overlay").show();
+				},
+				success: function(responseHTML){
+					$("#overlay").hide();	
+					$("#poll-content").html(responseHTML);				
+				}
+			});
+			
+		}
+	}
+    $(document).ready(function(){
+		show_poll();
+	});
+   </script>
+</body>
+</html>
