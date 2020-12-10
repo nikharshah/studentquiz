a8d013b1b103ed9914f8e2efcea719483c5f6e36
nikharshah <75773826+nikharshah@users.noreply.github.com>
2020-12-10 14:12:59 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>PHP Quiz Script</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div id="overlay">
+		<div>
+			<img src="loader.gif" width="64px" height="64px" />
+		</div>
+	</div>
+	<div class="poll-content-outer">
+		<div id="poll-content"></div>
+	</div>
+<script src="jquery-3.2.1.min.js"></script>
+<script>
