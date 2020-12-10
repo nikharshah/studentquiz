7015fe8b4d67b695eca00bbd50db72bf25bc2d03
nikharshah <75773826+nikharshah@users.noreply.github.com>
2020-12-10 15:40:11 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -10,4 +10,33 @@ class Connect {
 		$this->conn = $this->connectDB();
 	}
 	
+	function connectDB() {
+		$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+		return $conn;
+	}
 	
+	function runQuery($query) {
+		$result = mysqli_query($this->conn,$query);
+		while($row=mysqli_fetch_array($result)) {
+			$resultset[] = $row;
+		}
+		if(!empty($resultset))
+			return $resultset;
+	}
+	
+	function insertQuery($query) {
+	    mysqli_query($this->conn, $query);
+	    $insert_id = mysqli_insert_id($this->conn);
+	    return $insert_id;
+	}
+	
+	function getIds($query) {
+	    $result = mysqli_query($this->conn,$query);
+	    while($row=mysqli_fetch_array($result)) {
+	        $resultset[] = $row[0];
+	    }
+	    if(!empty($resultset))
+	        return $resultset;
+	}
+}
+?>
